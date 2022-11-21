7b78bfd410b66b953efd36c941cf0316ea33ff56
ziftgny <muhammed.guney@gazi.edu.tr>
2022-11-21 13:11:03 +0300

    yorum

--- string_karakter_secme.c
+++ string_karakter_secme.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include<conio.h>
-#include<stdlib.h>
+#include<stdlib.h>//systm("cls") icin gerekli kütüphane
 int main()
 {
 
     char deneme[][10] ={"selam","merhaba"};
         
        int dene;
-         for(int i =0;i<sizeof(deneme)/sizeof(deneme[0]);i++){
+         for(int i =0;i<sizeof(deneme)/sizeof(deneme[0]);i++){//sizeofdeneme = 20 ,sizeofdeneme[0]=10
 
                 for(int j =0;deneme[i][j];j++){ //deneme[i][j] nin degersel olarak karsılıgı oldugu surece devam et
                                                 //deneme[0][5] in bir degeri olmadıgı için döngü burda kırılır
                 
-                    printf("%c\n",deneme[i][j]);
-                    deneme[i][j]=='h'?system("cls"):1;
+                    printf("%c\n",deneme[i][j]);// %s yerine %c kullanımına dikkat
+                    deneme[i][j]=='h'?system("cls"):1;//system("cls") terminal ekranini temizler
 
         }
         
