aaa36bf79da0b1b3520611520d579623eff71bd6
ziftgny <muhammed.guney@gazi.edu.tr>
2022-11-21 11:34:48 +0300

    yorum ekleme

--- string_karakter_secme.c
+++ string_karakter_secme.c
@@ -10,7 +10,8 @@ int main()
        int dene;
          for(int i =0;i<sizeof(deneme)/sizeof(deneme[0]);i++){
 
-                for(int j =0;deneme[i][j];j++){
+                for(int j =0;deneme[i][j];j++){ //deneme[i][j] nin degersel olarak karsılıgı oldugu surece devam et
+                                                //deneme[0][5] in bir degeri olmadıgı için döngü burda kırılır
                 
                     printf("%c\n",deneme[i][j]);
                     deneme[i][j]=='h'?system("cls"):1;
